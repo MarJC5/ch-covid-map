c6a8068fc070bc4590719e33ee823037f6b1d4b2
Martin Jean-Christio <j.christio.martin@gmail.com>
2020-03-31 02:16:50 +0200

    Cases display style

--- src/components/infoCard.js
+++ src/components/infoCard.js
@@ -5,7 +5,7 @@ function InfoCard({canton, cases, deaths}) {
     <div className={`absolute card-info opacity-0 bg-white py-2 px-4 CARD-CH-${canton} flex flex-col`}>
         <h6 className="font-bold">{canton}</h6>
         <p className="text-sm">Cases: {cases}</p>
-        <p className="text-sm">Death: {deaths}</p>
+        {deaths == "" ? <p></p> : <p className="text-sm">Death: {deaths}</p>}
     </div>
   );
 }
